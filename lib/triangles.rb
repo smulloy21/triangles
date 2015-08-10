018030f2864bf2fcb6ff9be71886738bf090afe4
Shannon Mulloy & Rachael Berecka <student@epicodus.com>
2015-08-10 13:38:41 -0700

    add more specific triangles and images

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -14,16 +14,20 @@ class Triangle
   end
 
   define_method(:triangle_type) do
+    output = ""
     if triangle?()
+      if @side1*@side1 + @side2*@side2 == @side3*@side3 || @side1*@side1 + @side3*@side3 == @side2*@side2 || @side3*@side3 + @side2*@side2 == @side1*@side1
+        output += "right and "
+      end
       if @side1 == @side2 && @side1 == @side3
-        output = "equilateral."
+        output += "equilateral."
       elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-        output = "isosceles."
+        output += "isosceles."
       else
-        output = "scalene."
+        output += "scalene."
       end
     else
-      output = "not a triangle."
+      output += "not a triangle."
     end
   end
 end
