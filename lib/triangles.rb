5fe93a1b4e69a539e99cbb57192dc29cf1504731
Shannon Mulloy & Rachael Berecka <student@epicodus.com>
2015-08-10 10:00:10 -0700

    include methods to test if triangle and type of triangle

---
+++ lib/triangles.rb
@@ -0,0 +1,29 @@
+class Triangle
+  define_method(:initialize) do |side1, side2, side3|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  define_method(:triangle?) do
+    if @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side3 + @side1 > @side2
+      true
+    else
+      false
+    end
+  end
+
+  define_method(:triangle_type) do
+    if triangle?()
+      if @side1 == @side2 && @side1 == @side3
+        output = "equilateral"
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        output = "isosceles"
+      else
+        output = "scalene"
+      end
+    else
+      output = "Not a triangle."
+    end
+  end
+end
---
+++ spec/triangles_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('triangles')
+
+describe('Triangle') do
+  describe("#triangle?") do
+    it("returns true if a triangle") do
+      test_triangle = Triangle.new(6, 7, 5)
+      expect(test_triangle.triangle?()).to(eq(true))
+    end
+  end
+  describe("#triangle_type") do
+    it("returns the type of triangle") do
+      test_triangle = Triangle.new(5, 5, 5)
+      expect(test_triangle.triangle_type()).to(eq("equilateral"))
+    end
+  end
+end
